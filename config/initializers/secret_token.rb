4659cfb998fad19f7d5e5991fb8687fd0956f2a9
Tom <thomas.czernik@webreload.de>
2011-12-13 10:43:14 +0100

    Creating a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alarma::Application.config.secret_token = '53adca641cd61676fa701c6ec166d98b40dc1daf657012882f0c31cc90e4e7a0fd025c55fe9ac9adbb36cd80cc8137c35bd121990ef3201ed20c4f8a68ee610a'
